173dfc54374874d26ca5046e29029fbb0d35a610
Samuel Clay <samuel@ofbrooklyn.com>
2013-01-26 14:33:50 -0800

    OAuth for github, callback + authorize + model.

--- thedoclub/oauth/models.py
+++ thedoclub/oauth/models.py
@@ -1,3 +1,52 @@
+import bcrypt
 from django.db import models
+from vendor.github import GitHub
 
-# Create your models here.
+
+class GitHubUser(models.Model):
+    access_token = models.CharField(max_length=40, unique=True)
+    secret_token = models.CharField(max_length=128, db_index=True)
+    avatar_url = models.CharField(max_length=1024, null=True)
+    bio = models.CharField(max_length=1024, null=True)
+    blog = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255, null=True)
+    followers = models.IntegerField(null=True)
+    following = models.IntegerField(null=True)
+    hireable = models.BooleanField(default=False)
+    github_id = models.CharField(max_length=40, null=True)
+    location = models.CharField(max_length=255, null=True)
+    login = models.CharField(max_length=40, null=True)
+    name = models.CharField(max_length=128, null=True)
+    public_repos = models.IntegerField(null=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.secret_token:
+            self.secret_token = self.generate_secret_token()
+            
+        super(GitHubUser, self).save(*args, **kwargs)
+    
+    def generate_secret_token(self):
+        return bcrypt.hashpw(self.access_token, bcrypt.gensalt())
+    
+    def gh(self):
+        return GitHub(access_token=self.access_token)
+        
+    def fetch_user_info(self):
+        gh = self.gh()
+        user = gh.user.get()
+        
+        self.avatar_url = user['avatar_url']
+        self.bio = user['bio']
+        self.blog = user['blog']
+        self.email = user['email']
+        self.followers = user['followers']
+        self.following = user['following']
+        self.hireable = user['hireable']
+        self.github_id = user['id']
+        self.location = user['location']
+        self.login = user['login']
+        self.name = user['name']
+        self.public_repos = user['public_repos']
+        
+        self.save()
+
--- thedoclub/thedoclub/settings.py
+++ thedoclub/thedoclub/settings.py
@@ -113,10 +113,6 @@ INSTALLED_APPS = (
     'presentation',
     'oauth',
     'homepage',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
