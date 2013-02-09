b6823e5ee638b8791f07ba162849d7eee38ecff7
Samuel Clay <samuel@ofbrooklyn.com>
2013-02-09 12:11:09 -0800

    django-extensions.

--- thedoclub/thedoclub/settings.py
+++ thedoclub/thedoclub/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django-extensions',
     'djcelery',
     'presentation',
     'oauth',
